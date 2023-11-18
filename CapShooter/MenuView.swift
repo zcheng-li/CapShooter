725b1f88c273719095d582b44d5a3e17542b737a
Li Zhicheng <134947952+FaliLico@users.noreply.github.com>
2023-11-19 00:17:52 +0800

    导入了瓶盖模型

--- CapShooter/MenuView.swift
+++ CapShooter/MenuView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import RealityKit
+import RealityKitContent
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        }
     }
 }
 
