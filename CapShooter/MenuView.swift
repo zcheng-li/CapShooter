3a36b1de97f2d2774f961b13c27a85a041ab6346
Li Zhicheng <134947952+FaliLico@users.noreply.github.com>
2023-11-18 22:19:32 +0800

    init

---
+++ CapShooter/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  CapShooter
+//
+//  Created by 1234567 on 2023/11/18.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
